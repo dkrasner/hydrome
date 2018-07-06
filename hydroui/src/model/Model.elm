1f4d091c0c54966458cf6d8f7c5215e3a08b9601
dkrasner <krasnerdaniel@gmail.com>
2018-07-06 18:45:41 +0200

    adding model placeholders and display

--- hydroui/src/model/Model.elm
+++ hydroui/src/model/Model.elm
@@ -16,6 +16,13 @@ type alias Model =
     { area : String
     , controllerXY : Position
     , drag : Draggable.State String
+    , hydroModels : List HydroModel
+    }
+
+
+type alias HydroModel =
+    { name : String
+    , args : List String
     }
 
 
@@ -24,4 +31,45 @@ model =
     { area = "Landing"
     , controllerXY = Position 580 -250
     , drag = Draggable.init
+    , hydroModels =
+        [ hydroModel1
+        , hydroModel2
+        , hydroModel3
+        , hydroModel
+        , hydroModel
+        , hydroModel
+        , hydroModel
+        ]
+    }
+
+
+
+-- TODO: temp holder for models, get from api later
+
+
+hydroModel : HydroModel
+hydroModel =
+    { name = "HydroModel"
+    , args = [ "a1", "a2", "a3" ]
+    }
+
+
+hydroModel1 : HydroModel
+hydroModel1 =
+    { name = "HydroModel1"
+    , args = [ "a1", "a2", "a3" ]
+    }
+
+
+hydroModel2 : HydroModel
+hydroModel2 =
+    { name = "HydroModel2"
+    , args = [ "a1", "a2", "a3" ]
+    }
+
+
+hydroModel3 : HydroModel
+hydroModel3 =
+    { name = "HydroModel3"
+    , args = [ "a3", "a2", "a3" ]
     }
--- hydroui/src/view/Select.elm
+++ hydroui/src/view/Select.elm
@@ -3,6 +3,7 @@ module Select exposing (selectArea)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, placeholder, style, id, property, attribute, type_)
+import List
 import Model exposing (Model)
 import Update exposing (..)
 import Controller exposing (..)
@@ -12,12 +13,23 @@ selectArea : Model -> Html Msg
 selectArea model =
     let
         selectClass =
-            class "col col-4 h-75 m-1 border border-secondary rounded shadow"
+            class
+                "col col-4 h-75 m-1 border border-secondary rounded shadow selectarea"
+
+        modelBlocks =
+            List.map modelBlock model.hydroModels
     in
         div [ class "row h-100 justify-content-center align-items-center" ]
             [ controller model Nav
             , div [ selectClass ]
-                [ text "Select Model Area" ]
+                [ div [ class "row" ] modelBlocks ]
             , div [ selectClass ]
                 [ text "Select Domain Area" ]
             ]
+
+
+modelBlock : Model.HydroModel -> Html Msg
+modelBlock hydromodel =
+    div [ class "block" ]
+        [ div [] [ text hydromodel.name ]
+        ]

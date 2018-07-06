0fde7a38302084c74d74d576806bccc3ac715017
dkrasner <krasnerdaniel@gmail.com>
2018-07-06 20:34:45 +0200

    adding contorller to all areas

--- hydroui/src/view/Feedback.elm
+++ hydroui/src/view/Feedback.elm
@@ -5,11 +5,13 @@ import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, placeholder, style, id, property, attribute, type_)
 import Model exposing (Model)
 import Update exposing (..)
+import Controller exposing (..)
 
 
 feedbackArea : Model -> Html Msg
 feedbackArea model =
     div [ class "row h-100 justify-content-center align-items-center" ]
-        [ div []
+        [ controller model Nav
+        , div []
             [ text "Feedback Area" ]
         ]
--- hydroui/src/view/Stage.elm
+++ hydroui/src/view/Stage.elm
@@ -5,11 +5,13 @@ import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, placeholder, style, id, property, attribute, type_)
 import Model exposing (Model)
 import Update exposing (..)
+import Controller exposing (..)
 
 
 stageArea : Model -> Html Msg
 stageArea model =
     div [ class "row h-100 justify-content-center align-items-center" ]
-        [ div []
+        [ controller model Nav
+        , div []
             [ text "Stage Area" ]
         ]

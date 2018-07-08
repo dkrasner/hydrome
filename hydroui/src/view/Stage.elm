9766d3a9925b1fd7ddf0e03ad02f816618c65fd1
dkrasner <krasnerdaniel@gmail.com>
2018-07-08 19:04:12 +0200

    loading model and domain basic info from model + some css

--- hydroui/src/view/Stage.elm
+++ hydroui/src/view/Stage.elm
@@ -13,15 +13,37 @@ stageArea model =
     div [ class "row h-100 justify-content-center align-items-center stage" ]
         [ controller model Nav
         , div [ class "row w-100 h-50 justify-content-center" ]
-            [ div [ class "col col-5 mt-5 mr-4 ml-5 area" ]
-                [ text "Model Stage Area" ]
-            , div [ class "col col-5 mt-5 ml-4 mr-5 area" ]
-                [ text "Domain Stage Area" ]
+            [ div [ class "col col-5 mt-5 mr-4 ml-5 justify-content-center area" ]
+                [ stageModelArea model ]
+            , div [ class "col col-5 mt-5 ml-4 mr-5 justify-content-center area" ]
+                [ stageDomainArea model ]
             ]
         , div [ class "row w-100 h-50 justify-content-center" ]
-            [ div [ class "col col-5 mt-5 mr-4 ml-5 mb-5 area" ]
+            [ div [ class "col col-5 mt-5 mr-4 ml-5 mb-5 justify-content-center area" ]
                 [ text "Run Stage Area" ]
-            , div [ class "col col-5 mt-5 ml-4 mr-5 mb-5  area" ]
+            , div [ class "col col-5 mt-5 ml-4 mr-5 mb-5 justify-content-center area" ]
                 [ text "Scheduler Stage Area" ]
             ]
         ]
+
+
+{-| The following control the layout of their repsecitve areas
+-}
+stageModelArea : Model -> Html Msg
+stageModelArea model =
+    case model.stageModel of
+        Nothing ->
+            h3 [] [ text "No model selected" ]
+
+        Just stageModel ->
+            h3 [] [ text stageModel.name ]
+
+
+stageDomainArea : Model -> Html Msg
+stageDomainArea model =
+    case model.stageDomain of
+        Nothing ->
+            h3 [] [ text "No domain selected" ]
+
+        Just stageDomain ->
+            h3 [] [ text stageDomain.name ]
